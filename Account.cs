d7777a1b5b68fe32c521e4f0123aa9d3497bc364
eyael <47112888+eyael@users.noreply.github.com>
2021-05-04 15:00:38 -0400

    .Net commit

---
+++ Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankApplication
+{
+    public class Account
+    {
+        public string AccountNumber { get; set; }
+        public string Balance { get; set; }
+        public string AccountType { get; set; }
+    }
+}
